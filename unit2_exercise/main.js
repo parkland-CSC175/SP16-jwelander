ed6cbc74b966c9f6b81224e34a8d2f9b72ac933a
welander <jimwelander316@hotmail.com>
2016-02-08 10:58:36 -0600

    exercise unit 2

---
+++ unit2_exercise/main.js
@@ -0,0 +1,79 @@
+//4a
+console.log("Hello my name is Jim Welander");
+//4B
+console.log("This is a 'single quoted statement' and this is a \"double quoed statement\"");
+//5a
+console.log("2 plus 2 equals: " + (2 + 2));
+//5b
+console.log("56 divided by 7 equals: " + (56 / 7));
+//5c
+console.log("42.6 times 18.3 equals: " + (42.6 * 18.3));
+//5d
+console.log("83.97 minus 34.86 equals: " + (83.97 - 34.86));
+//5e
+console.log("49 mod 8 equals: " + (49 % 8));
+//6a
+console.log("The answer is: " + (5 + 4 + 3 / 2 * 6 - 1));
+//6b
+console.log("The answer is: " + (29 * 75 + 32 / 8));
+//6c
+console.log("The answer is: " + (((42 + 89) * 3) / (72 - 36)));
+//6d
+console.log("The answer is: " + (4 % 3));
+//6e
+console.log("The answer is: $" + (33.00 - 15.87));
+//7a
+console.log("The type of '56' is: " + typeof (56));
+//7b
+console.log("The type of '43.211' is: " + typeof (43.211));
+//7c
+console.log("The type of '\"2\"' is: " + typeof ("2"));
+//7d
+console.log("The type of '\"two\"' is: " + typeof ("two"));
+//7e
+console.log("The type of 'new Date()' is: " + typeof (new Date()));
+//7f
+console.log("The type of '[]' is: " + typeof ([]));
+//7g
+console.log("The type of '{}' is: " + typeof ({}));
+//7h
+console.log("The type of 'True' is: " + typeof (True));
+//7i
+console.log("The type of 'False' is: " + typeof (False));
+//7j
+console.log("The type of 'Null' is: " + typeof (Null));
+//7k
+console.log("The type of 'Undefined' is: " + typeof (Undefined));
+//7L
+console.log("The type of 'NaN' is: " + typeof (NaN));
+//7m
+console.log("The type of '0' is: " + typeof (0));
+//7n
+console.log("The type of '\"0\"' is: " + typeof ("0"));
+//7o
+console.log("The type of '\"\"' is: " + typeof (""));
+//7p
+console.log("The type of '\"\"||1' is: " + typeof (""||1));
+//7q
+console.log("The type of '3>2' is: " + typeof (3 > 2));
+//7r
+console.log("The type of '2>3' is: " + typeof (2 > 3));
+//7s
+console.log("The type of 'new Number(\"some text\")' is: " + typeof (new Number("some text")));
+
+var foo = 2;
+var bar = 3;
+var total;
+total = foo + bar;
+total = (foo * bar) + total;
+total = total / bar;
+total = total * total;
+console.log("The final total is: " + total + " , when the starting values are 'foo': " + foo + " and 'bar': " + bar);
+
+
+
+
+
+
+
+
