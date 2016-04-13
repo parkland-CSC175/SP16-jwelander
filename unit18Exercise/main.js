f09ab7a8ad3017c2e8a6d700791618b7b2fb9f81
welander <jimwelander316@hotmail.com>
2016-04-13 12:56:12 -0500

    Exercise 18, all but remove working
    All works except updating observableArray following deletion from data
    array.

--- unit18Exercise/main.js
+++ unit18Exercise/main.js
@@ -2,5 +2,45 @@
 $(document).ready(function(){
     
     console.log("hello world");
+    var carJSON;
+    var carDataUrl = "https://parkland-CSC175.github.io/csc175data/cars/car-makes.json";
+    var carData = [];
+     $.getJSON(carDataUrl, function(data){
+        data.forEach(function(aCarObject){
+            carData.push(
+             {make_id: aCarObject.make_id,
+                make_display: aCarObject.make_display,
+                make_is_common: aCarObject.make_is_common,
+                make_country: aCarObject.make_country}
+               );
+        });
+         
+  var viewModel = {   
+        carBrands: ko.observableArray(carData),
+        removeAnItem: function(){
+            console.log(carData);
+            for (var i = 0; i < carData.length; i++){
+                if(carData[i].make_display == this.make_display){
+                    carData.splice(i,1);
+                }
+            };       
+            console.log(carData);      
+            
+        },
+        addToItems: function() {
+            console.log(this.carBrands);
+            this.carBrands.push({
+                make_id: "default",
+                make_display: $("#brand-text").val(),
+                make_is_common: "1",
+                make_country: $("#country-text").val()
+            });
+            this.carBrands.sort((function (left, right) { return left.make_display == right.make_display ? 0 : (left.make_display < right.make_display ? -1 : 1) }));
+           }   
+    } 
+    ko.applyBindings(viewModel);
 
+    
+  });
+ 
 });
--- unit18Notes/main.js
+++ unit18Notes/main.js
@@ -1,6 +1,41 @@
 // https://learn.jquery.com/using-jquery-core/document-ready/
-$(document).ready(function(){
-    
-    console.log("hello world");
+$(document).ready(function() {
+
+    function Car(brandParam, engineParam) {
+        this.brand = brandParam || "Chevrolet";
+        this.engine = engineParam || "5.7L V8";
+
+        this.honk = function() {
+            console.log("Hooooonk");
+        }
+    }
+
+    var defaultCar = new Car();
+    var customerCar = new Car("Ford", "1.8L I4");
+
+    /////////////////////////
+    /// knockout notes
+
+
+    var viewModel = {
+        heading: ko.observable("Jim's Heading"),
+
+        items: ko.observableArray([]),
+        updateHeading: function() {
+            var newHeading = "The time is " + Date.now();
+            this.heading(newHeading);
+        },
+        addToItems: function() {
+            this.items.push({
+                name: "Some Item " + Date.now(),
+                completed: false
+            });
+        }
+
+    };
+    // Links viewModel to the html
+    ko.applyBindings(viewModel);
+
+
 
 });
