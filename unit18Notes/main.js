a755bf788d7e86f12c68a545b36cfb635b182fd8
welander <jimwelander316@hotmail.com>
2016-04-13 14:45:41 -0500

    Revert "Exercise 18, all but remove working"
    This reverts commit f09ab7a8ad3017c2e8a6d700791618b7b2fb9f81.

--- unit18Notes/main.js
+++ unit18Notes/main.js
@@ -1,41 +1,6 @@
 // https://learn.jquery.com/using-jquery-core/document-ready/
-$(document).ready(function() {
-
-    function Car(brandParam, engineParam) {
-        this.brand = brandParam || "Chevrolet";
-        this.engine = engineParam || "5.7L V8";
-
-        this.honk = function() {
-            console.log("Hooooonk");
-        }
-    }
-
-    var defaultCar = new Car();
-    var customerCar = new Car("Ford", "1.8L I4");
-
-    /////////////////////////
-    /// knockout notes
-
-
-    var viewModel = {
-        heading: ko.observable("Jim's Heading"),
-
-        items: ko.observableArray([]),
-        updateHeading: function() {
-            var newHeading = "The time is " + Date.now();
-            this.heading(newHeading);
-        },
-        addToItems: function() {
-            this.items.push({
-                name: "Some Item " + Date.now(),
-                completed: false
-            });
-        }
-
-    };
-    // Links viewModel to the html
-    ko.applyBindings(viewModel);
-
-
+$(document).ready(function(){
+    
+    console.log("hello world");
 
 });
